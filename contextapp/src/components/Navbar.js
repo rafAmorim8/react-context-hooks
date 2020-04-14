a02570ce13d11ca7bf45b987d48e835b20709ff2
RafaelAmorim <rafael_amorim30@hotmail.com>
2020-04-14 09:25:41 -0700

    Adds context provider

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext'
 
 class Navbar extends Component {
-    render() { 
+    static contextType = ThemeContext;
+    render() {
+        const {isLightTheme, light, dark } = this.context; 
+        const theme = isLightTheme ? light : dark;
         return (
-            <nav>
+            <nav style={{ background: theme.bg, color: theme.syntax}}>
                 <h1>Context App</h1>
                 <ul>
                     <li>Home</li>
