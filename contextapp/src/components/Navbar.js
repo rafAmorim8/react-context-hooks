36128cd1e68219fbb8cfd187c6f079b908626f1b
RafaelAmorim <rafael_amorim30@hotmail.com>
2020-04-14 10:09:29 -0700

    Adds auth contect to navbar

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext'
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
     render() {
         return (
-            <ThemeContext.Consumer>{(context) => {
-                const {isLightTheme, light, dark } = context; 
-                const theme = isLightTheme ? light : dark;
-                return(
-                    <nav style={{ background: theme.bg, color: theme.syntax}}>
-                        <h1>Context App</h1>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </nav>
-                )  
-            }}</ThemeContext.Consumer>
+            <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(themeContext) => {
+                    const {isLightTheme, light, dark } = themeContext;
+                    const { isAuthenticated, toggleAuth } = authContext; 
+                    const theme = isLightTheme ? light : dark;
+                    return(
+                        <nav style={{ background: theme.bg, color: theme.syntax}}>
+                            <h1>Context App</h1>
+                            <div onClick={toggleAuth}>
+                                { isAuthenticated ? 'Logged in' : 'Logged out' }
+                            </div>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    )  
+                }}</ThemeContext.Consumer>
+            )}</AuthContext.Consumer>
         );
     }
 }
